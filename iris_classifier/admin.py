9b96d7eea36cf94376de563daa690926b414db86
alidumn2 <alidumn2@gmail.com>
2025-12-25 00:37:40 +0300

    changed

--- iris_classifier/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Collector, Iris
-
-admin.site.register(Collector)
-admin.site.register(Iris)
-
--- iris_classifier/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class IrisClassifierConfig(AppConfig):
-    name = 'iris_classifier'
